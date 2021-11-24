d1dfae1924a5abf807a2c1c31ee43a768b2fa388
Anne Isabelle Rodrigues de Carvalho <anneisabelle.rodrigues@outlook.com>
2021-11-24 12:57:58 -0300

    criando rotas da API com The movie database

--- match-ur-date-back/web/api.py
+++ match-ur-date-back/web/api.py
@@ -1,9 +1,35 @@
 from flask import Blueprint, Flask, request, Response
+import requests, json, re
 from db import initialize_db
 from models import Movie
+from constants import Endpoint
 
 api = Blueprint('api', __name__)
 
 '''
     Aqui vão as rotas de api
 '''
+
+@api.route('/get_data_by_id', methods=['GET'])
+def get_data_by_id(movie_id):
+    url = Endpoint.GET_DATA_BY_ID.value
+    url = url.replace('%Movie_id%', movie_id)
+    response = requests.get(url)
+    if response.status_code == 200:
+        array = response.json()
+        text = json.dumps(array)
+        return(text)
+    else:
+        return("error")
+
+@api.route('/', methods=['GET'])
+def get_movies_by_genre(genre):
+    url = Endpoint.GET_MOVIES_BY_GENRE.value
+    url = url.replace('%genre%', genre)
+    response = requests.get(url)
+    if response.status_code == 200:
+        array = response.json()
+        text = json.dumps(array)
+        return(text)
+    else:
+        return("error")
