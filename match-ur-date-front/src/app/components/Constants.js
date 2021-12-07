4be4fe0fa80765e266880107f0b2df727e017cd5
Taís Christofani <taiskc@gmail.com>
2021-12-06 23:21:32 -0300

    Adição de constantes

--- match-ur-date-front/src/app/components/Constants.js
+++ match-ur-date-front/src/app/components/Constants.js
@@ -1,3 +1,9 @@
-export const systemDescription = "Usar o match-ur-date é simples como todo date deveria ser. Alguém cria uma sessão e compartilha o código com seu parceiro e este ingressa na sessão. As opções de filme começarão a aparecer para cada um quando ambos estiverem prontos. Quem quiser assistir a opção mostrada passa pra direita ou, caso contrário, pra esquerda. Quando der o match a gente avisa aos dois e aí é só aproveitar com o seu par! <3";
-export const logoDescription = "Dois ingressos com meio coração se completando.";
+export const text = {
+    description: "Usar o match-ur-date é simples como todo date deveria ser. Alguém cria uma sessão e compartilha o código com seu parceiro e este ingressa na sessão. As opções de filme começarão a aparecer para cada um quando ambos estiverem prontos. Quem quiser assistir a opção mostrada passa pra direita ou, caso contrário, pra esquerda. Quando der o match a gente avisa aos dois e aí é só aproveitar com o seu par! <3",
+    logo: "Dois ingressos com meio coração se completando.",
+    create: "Criar sessão",
+    enter: "Ingressar em sessão",
+    cancel: "Cancelar",
+    ok: "OK"
+}
 
