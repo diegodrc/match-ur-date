b1f812350491838853165f367406f5e7ad5ac6e4
Taís Christofani <taiskc@gmail.com>
2021-12-06 22:21:03 -0300

    Criação arquivo de constantes

---
+++ match-ur-date-front/src/app/components/Constants.js
@@ -0,0 +1,3 @@
+export const systemDescription = "Usar o match-ur-date é simples como todo date deveria ser. Alguém cria uma sessão e compartilha o código com seu parceiro e este ingressa na sessão. As opções de filme começarão a aparecer para cada um quando ambos estiverem prontos. Quem quiser assistir a opção mostrada passa pra direita ou, caso contrário, pra esquerda. Quando der o match a gente avisa aos dois e aí é só aproveitar com o seu par! <3";
+export const logoDescription = "Dois ingressos com meio coração se completando.";
+
