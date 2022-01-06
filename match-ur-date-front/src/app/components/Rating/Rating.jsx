137a79a5bf19bd09bdc012fe603eca60ce039baf
Taís Christofani <taiskc@gmail.com>
2022-01-05 23:58:09 -0300

    Tela de avaliação

--- match-ur-date-front/src/app/components/Rating/Rating.jsx
+++ match-ur-date-front/src/app/components/Rating/Rating.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { useState, useRef } from 'react';
 import { Button, Card, CardActionArea, CardMedia, CardContent, Grid, Typography } from '@mui/material';
 import Logo from '../../assets/images/logo.png';
-import styles from './Initial.styles';
-import styles from './Rating.styles';
-import SensorDoorIcon from '@mui/icons-material/SensorDoor';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import styles from './../Initial/Initial.styles';
+import SensorDoorIcon from '@mui/icons-material/CheckCircle';
+import AddCircleIcon from '@mui/icons-material/Cancel';
 import * as Constants from '../Constants';
 import GenericModal from '../utils/GenericModal';
 import { useDispatch } from 'react-redux';
@@ -19,38 +18,36 @@ const Rating = () => {
 
     const dispatch = useDispatch();
 
-    const cancelEnterModal = () => setOpenEnterModal(false);
-
     // const cancelCreateModal = () => setOpenCreateModal(false);
     
     const showMatch = () => {
         dispatch(changeTela('match'));
     };
 
-    const enterModal = (
-        <Typography sx={styles.text}>Criar sessão com a categoria ${chosenCategory}?</Typography>
-    );
+    const goToNextMovie = () => {
+      setMovie(prevState => prevState + 1);
+    };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '50px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '50px' }}>
             <Typography variant="h4" style={{ fontFamily: theme.typography.fontFamily }}>Você gostaria de assistir a esse filme?</Typography>
-    <Card sx={{ maxWidth: 345, maxWidth: '100%', minHeight: '100%' }}>
+    <Card sx={{ maxWidth: 345, margin: 3 }}>
         <CardMedia
           component="img"
           image="https://i0.wp.com/jardimpamplonashopping.com.br/wp-content/uploads/2020/03/image-from-rawpixel-id-94019-original_Easy-Resize.com_.jpg?fit=884%2C590&ssl=1"
         />
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" sx={{ mt: 3 }}>
             {mockMovies[movie]}
           </Typography>
           <CardContent>
-          <Typography gutterBottom variant="h4" component="div">
+          <Typography gutterBottom variant="h6" component="div">
             Sinopse: Eis aqui um campo para a sinopse.
           </Typography>
         </CardContent>
     </Card>
     <div style={styles.options}>
-                <Button startIcon={<AddCircleIcon />} variant="contained" sx={styles.buttons} onClick={showMatch}>Sim</Button>
-                <Button startIcon={<SensorDoorIcon />} variant="contained" sx={styles.buttons} onClick={goToNextMovie}>Não</Button>
+                <Button startIcon={<AddCircleIcon />} size="large" variant="contained" sx={styles.buttons} style={{ background: 'green', marginTop: '20px' }} onClick={showMatch}>Sim</Button>
+                <Button startIcon={<SensorDoorIcon />} size="large" variant="contained" sx={styles.buttons} style={{ background: 'red', marginTop: '20px' }} onClick={goToNextMovie}>Não</Button>
             </div>
         </div>
     );
